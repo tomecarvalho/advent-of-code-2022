e85136bff34bd72de0ea4248d436b88c865fd47d
tomecarvalho <tomecarvalho@proton.me>
2022-12-08 22:59:44 +0100

    Day 4 part 2

--- 4/camp_cleanup.py
+++ 4/camp_cleanup.py
@@ -3,6 +3,14 @@ def contains(pair):
     return (x0 <= x1 and y0 >= y1) or (x1 <= x0 and y1 >= y0)
 
 
+def overlaps(pair):
+    (x0, y0), (x1, y1) = pair
+    if x0 <= x1 and y0 <= y1:
+        return not y0 < x1
+    else:
+        return not y1 < x0
+
+
 with open("input.txt", "r") as f:
     pairs = [line.split(",") for line in f.read().splitlines()]
     pairs_int = []
@@ -12,5 +20,7 @@ with open("input.txt", "r") as f:
             n1, n2 = el.split("-")
             pair_int.append((int(n1), int(n2)))
         pairs_int.append(pair_int)
-    count = sum(contains(pair) for pair in pairs_int)
-    print(f"Solution #1: {count}")
+    count_contains = sum(contains(pair) for pair in pairs_int)
+    count_overlaps = sum(overlaps(pair) for pair in pairs_int)
+    print(f"Solution #1: {count_contains}")
+    print(f"Solution #2: {count_overlaps}")
