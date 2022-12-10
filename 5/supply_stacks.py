66a9a795cd0490b25b9733bf7c35fdbda1f98af1
tomecarvalho <tomecarvalho@proton.me>
2022-12-10 14:51:24 +0100

    Day 5 part 2

--- 5/supply_stacks.py
+++ 5/supply_stacks.py
@@ -9,30 +9,56 @@ def determine_block_len(init_pos):
     return None
 
 
-with open("input.txt", "r") as f:
-    init_pos, moves = f.read().split("\n\n")
-    init_pos = init_pos.splitlines()
-    moves = moves.splitlines()
-    block_len = determine_block_len(init_pos)
-
-    stack_names = init_pos.pop().split()
-    names_dict = {name: i for i, name in enumerate(stack_names)}
-    n_stacks = len(stack_names)
-    stacks = [[] for _ in range(n_stacks)]
-
-    for line in init_pos[::-1]:
-        n = block_len + 1
-        blocks = tuple(block.strip() for block in (
-            line[i:i+n] for i in range(0, len(line), n)))
-        for i, block in enumerate(blocks):
-            if block:
-                stacks[i].append(block)
-
-    for move in moves:
-        move_split = move.split()
-        n = int(move_split[1])
-        src_stack = stacks[names_dict[move_split[3]]]
-        dest_stack = stacks[names_dict[move_split[5]]]
+def move_crates(n, src_stack, dest_stack, multiple_at_once=False):
+    if multiple_at_once:
+        dest_stack += src_stack[-n:]
+        del src_stack[-n:]
+    else:
         dest_stack += [src_stack.pop() for _ in range(n)]
 
-    print(f"Solution #1: '{''.join(stack[-1][1] if stack else ' ' for stack in stacks)}'")
+
+def solve():
+    with open("input.txt", "r") as f:
+        init_pos, moves = f.read().split("\n\n")
+        init_pos = init_pos.splitlines()
+        moves = moves.splitlines()
+        block_len = determine_block_len(init_pos)
+
+        stack_names = init_pos.pop().split()
+        names_dict = {name: i for i, name in enumerate(stack_names)}
+        n_stacks = len(stack_names)
+        stacks_1 = [[] for _ in range(n_stacks)]
+        stacks_2 = [[] for _ in range(n_stacks)]
+
+        for line in init_pos[::-1]:
+            n = block_len + 1
+            blocks = tuple(block.strip() for block in (
+                line[i:i+n] for i in range(0, len(line), n)))
+            for i, block in enumerate(blocks):
+                if block:
+                    for stacks in (stacks_1, stacks_2):
+                        stacks[i].append(block)
+
+        for move in moves:
+            move_split = move.split()
+            n = int(move_split[1])
+            src_stack_idx = names_dict[move_split[3]]
+            dest_stack_idx = names_dict[move_split[5]]
+
+            src_stack_1 = stacks_1[src_stack_idx]
+            dest_stack_1 = stacks_1[dest_stack_idx]
+
+            src_stack_2 = stacks_2[src_stack_idx]
+            dest_stack_2 = stacks_2[dest_stack_idx]
+
+            move_crates(n, src_stack_1, dest_stack_1)
+            move_crates(n, src_stack_2, dest_stack_2, True)
+
+        def stacks_on_top_str(stacks):
+            return "".join(stack[-1][1] if stack else " " for stack in stacks)
+
+        for i, stacks in enumerate((stacks_1, stacks_2)):
+            print(f"Solution #{i+1}: {stacks_on_top_str(stacks)}")
+
+
+solve()
